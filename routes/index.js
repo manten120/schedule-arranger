628f5e9412bb283a28954638813f14d9434406ce
stacru <hamaw8888@gmail.com>
2021-01-03 04:41:48 +0900

    fix typo

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+const express = require('express');
+const router = express.Router();
 const Schedule = require('../models/schedule');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   const title = '予定調整くん';
-  
   if (req.user) {
     Schedule.findAll({
       where: {
