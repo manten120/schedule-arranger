0bf67e2fa497f704d01d11ec06fb9b425554d247
stacru <hamaw8888@gmail.com>
2021-01-07 22:58:35 +0900

    console.logを削除

--- app.js
+++ app.js
@@ -117,7 +117,6 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  console.log('エラー')
   // render the error page
   res.status(err.status || 500);
   res.render('error');
